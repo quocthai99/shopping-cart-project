114a274f70e0e0aba70391cf043af1299c80bba7
quocthai99 <quocthai5101999@gmail.comn>
2022-08-15 19:41:00 +0700

    homepage finish

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 import Helmet from '../components/Helmet'
 import HeroSlider from '../components/HeroSlider'
+import Section, { SectionTitle, SectionBody } from '../components/Section'
+import PolicyCard from '../components/PolicyCard'
+import ProductCard from '../components/ProductCard'
+
+import Grid from '../components/Grid'
+import { Link } from 'react-router-dom'
 
 import heroSliderData from '../assets/fake-data/hero-slider'
+import policy from '../assets/fake-data/policy'
+import productData from '../assets/fake-data/products'
+import banner from '../assets/images/banner.png'
 
 function Home() {
   return (
@@ -16,6 +25,130 @@ function Home() {
         timeOut={3000}
       />
       {/* End Hero Slider */}
+
+      {/* policy section */}
+      <Section>
+        <SectionBody>
+          <Grid
+            col={4}
+            mdCol={2}
+            smCol={1}
+            gap={20}
+          >
+            {
+              policy.map((item, index) =>
+                <Link to="/policy" key={index}>
+                  <PolicyCard
+                    name={item.name}
+                    description={item.description}
+                    icon={item.icon}
+                  />
+                </Link>)
+            }
+          </Grid>
+        </SectionBody>
+      </Section>
+      {/* end policy section */}
+
+      {/* best selling section */}
+      <Section>
+        <SectionTitle>
+          top sản phẩm bán chạy trong tuần
+        </SectionTitle>
+        <SectionBody>
+          <Grid
+            col={4}
+            mdCol={2}
+            smCol={1}
+            gap={20}
+          >
+            {
+              productData.getProducts(4).map((item, index) => (
+                <ProductCard
+                  key={index}
+                  img01={item.image01}
+                  img02={item.image02}
+                  name={item.title}
+                  price={Number(item.price)}
+                  slug={item.slug}
+                />
+              ))
+            }
+
+          </Grid>
+        </SectionBody>
+      </Section>
+      {/* end best selling section */}
+
+      {/* new arrival section */}
+      <Section>
+        <SectionTitle>
+          sản phẩm mới
+        </SectionTitle>
+        <SectionBody>
+          <Grid
+            col={4}
+            mdCol={2}
+            smCol={1}
+            gap={20}
+          >
+            {
+              productData.getProducts(8).map((item, index) => (
+                <ProductCard
+                  key={index}
+                  img01={item.image01}
+                  img02={item.image02}
+                  name={item.title}
+                  price={Number(item.price)}
+                  slug={item.slug}
+                />
+              ))
+            }
+
+          </Grid>
+        </SectionBody>
+      </Section>
+      {/* end new arrival section */}
+
+      {/* banner */}
+            <Section>
+              <SectionBody>
+                <Link to="/catalog">
+                  <img src={banner} alt="banner" />
+                </Link>
+              </SectionBody>
+            </Section>
+      {/* end banner */}
+
+      {/* popular product section */}
+      <Section>
+        <SectionTitle>
+          phổ biến
+        </SectionTitle>
+        <SectionBody>
+          <Grid
+            col={4}
+            mdCol={2}
+            smCol={1}
+            gap={20}
+          >
+            {
+              productData.getProducts(12).map((item, index) => (
+                <ProductCard
+                  key={index}
+                  img01={item.image01}
+                  img02={item.image02}
+                  name={item.title}
+                  price={Number(item.price)}
+                  slug={item.slug}
+                />
+              ))
+            }
+
+          </Grid>
+        </SectionBody>
+      </Section>
+      {/* end popular product section */}
     </Helmet>
   )
 }
